02010bb6af8ab3685a244d89a2f750ebd2e225f0
stenerInnethic <steiner.innethic@gmail.com>
2025-07-15 12:01:48 -0500

    fix: fix prisma and docker work

--- src/settings/app.module.ts
+++ src/settings/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { GModelsModule } from 'src/models-service/settings/app.module';
 import { GUserModule } from 'src/user-service';
 
 @Module({
-  imports: [GUserModule],
+  imports: [
+    GUserModule, 
+    GModelsModule
+  ],
   providers: [],
 })
 export class MainModule {}
