78c7439f8d45160414edbafcda5d11efb3a3a40c
EstebanInnethic <esteban.innethic@gmail.com>
2025-07-18 10:57:11 -0500

    refactor: add microservice finance using incomes and payment methods

--- src/incomes-service/settings/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IncomesModule } from '../incomes/incomes.module';
-
-@Module({
-  imports: [IncomesModule],
-})
-export class GIncomesModule { }
--- src/settings/app.module.ts
+++ src/settings/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { GIncomesModule } from 'src/incomes-service/settings/app.module';
 import { GLibraryModule } from 'src/library-service/settings/app.module';
 import { GUserModule } from 'src/user-service';
-import { GPaymentsModule } from '../payments-service/settings/app.module';
+import { GFinanceModule } from 'src/finance-service/settings/app.module';
 
 @Module({
   imports: [
     GUserModule,
     GLibraryModule,
-    GIncomesModule,
-    GPaymentsModule
+    GFinanceModule
   ],
   providers: [],
 })
